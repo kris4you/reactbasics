0f59a99cdc191766efeeecbd9e54694334a47599
kris4you <khotha82@gmail.com>
2019-12-09 19:41:50 -0500

    test

---
+++ src/main/java/com/example/demo/ReactbasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactbasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactbasicsApplication.class, args);
+	}
+
+}
